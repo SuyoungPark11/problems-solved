3010d87acdb505ec33e1f4bbc13dc517fc2b87e9
SuyoungPark11 <87575830+SuyoungPark11@users.noreply.github.com>
2024-08-20 23:51:26 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 900 ms, Memory: 105400 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -8,8 +8,12 @@ for _ in range(n):
 
 D = [[0] * (n+1) for _ in range(n+1)]
 for i in range(1, n+1):
-    for j in range(1, n+1):
-        D[i][j] = a[i][j] + D[i-1][j] + D[i][j-1] - D[i-1][j-1]
+    D[i][1] = D[i-1][1] + a[i][1]
+for j in range(1, n+1):
+    D[1][j] = D[1][j-1] + a[1][j]
+for i in range(2, n+1):
+    for j in range(2, n+1):
+        D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + a[i][j]
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
