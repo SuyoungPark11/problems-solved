324baefd5b76bee44dff042fa33763d76f501727
SuyoungPark11 <87575830+SuyoungPark11@users.noreply.github.com>
2024-08-20 18:55:34 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 212 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,13 +1,12 @@
 import sys
 
-input = sys.stdin.readline
-suNo,quizNo = map(int, input().split())
-numbers = list(map(int, input().split()))
+suNo,quizNo = map(int, sys.stdin.readline().split())
+numbers = list(map(int, sys.stdin.readline().split()))
 prifix_sum = [0]
 temp = 0
 for i in numbers:
     temp = temp + i
     prifix_sum.append(temp) 
 for i in range(quizNo):
-    s, e = map(int, input().split())
+    s, e = map(int, sys.stdin.readline().split())
     print(prifix_sum[e]-prifix_sum[s-1]) 
