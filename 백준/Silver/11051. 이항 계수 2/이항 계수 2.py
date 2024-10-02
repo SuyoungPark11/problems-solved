c1c290101e338c5442f5a92d5b4fe270333b0ac3
SuyoungPark11 <87575830+SuyoungPark11@users.noreply.github.com>
2024-10-02 23:27:07 +0900

    [Silver II] Title: 이항 계수 2, Time: 228 ms, Memory: 88152 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+C = [[0 for _ in range(N+1)] for _ in range(N+1)]
+
+for i in range(0, N+1):
+    C[i][1] = i
+    C[i][0] = 1
+    C[i][i] = 1
+    
+for i in range(2, N+1):
+    for j in range(1, i):
+        C[i][j] = C[i-1][j] + C[i-1][j-1]
+
+print(C[N][K] % 10007)
