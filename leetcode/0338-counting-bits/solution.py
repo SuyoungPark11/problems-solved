bddcbb38382ec4cf599b1d5006853bf14c5650f6
SuyoungPark11 <87575830+SuyoungPark11@users.noreply.github.com>
2024-11-06 08:25:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.6 MB (88.40%)

---
+++ leetcode/0338-counting-bits/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n+1)
+        if n >= 2:
+            ans[1] = 1
+            for i in range(2, n+1):
+                ans[i] = ans[i // 2] + i % 2
+        elif n == 1:
+            ans[1] = 1
+        return ans       
+
+        
