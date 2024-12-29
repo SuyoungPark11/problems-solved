1d63df4eed58870a375a8fcd03f1bf655bbc4d8a
SuyoungPark11 <87575830+SuyoungPark11@users.noreply.github.com>
2024-12-29 09:28:38 +0000

    [LeetCode Sync] Runtime - 1 ms (60.33%), Memory - 18.1 MB (8.65%)

---
+++ leetcode/3581-the-two-sneaky-numbers-of-digitville/solution.py
@@ -0,0 +1,5 @@
+from collections import Counter
+
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        return [k for k, v in Counter(nums).items() if v != 1]         
