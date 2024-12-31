ffc2d3d0c929aa06005c6a294203a81b07f8d10c
SuyoungPark11 <87575830+SuyoungPark11@users.noreply.github.com>
2024-12-31 05:51:02 +0000

    [LeetCode Sync] Runtime - 203 ms (27.20%), Memory - 14.7 MB (7.20%)

---
+++ leetcode/1786-count-the-number-of-consistent-strings/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def countConsistentStrings(self, allowed, words):
+        """
+        :type allowed: str
+        :type words: List[str]
+        :rtype: int
+        """
+        return sum(all(c in allowed for c in word) for word in words)
