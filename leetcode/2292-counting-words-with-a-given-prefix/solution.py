28ccd257741804ea31edc0c3df2ecf1419c01609
SuyoungPark11 <87575830+SuyoungPark11@users.noreply.github.com>
2025-01-09 13:53:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (17.22%)

---
+++ leetcode/2292-counting-words-with-a-given-prefix/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def prefixCount(self, words, pref):
+        """
+        :type words: List[str]
+        :type pref: str
+        :rtype: int
+        """
+        return sum(w.find(pref) == 0 for w in words)
