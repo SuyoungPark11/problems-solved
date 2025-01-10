a1af1c44f94dfd7a7a5a98dd13fb57ea7da32988
SuyoungPark11 <87575830+SuyoungPark11@users.noreply.github.com>
2025-01-10 10:19:49 +0900

    [Silver V] Title: 영화감독 숌, Time: 652 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+result = 666
+count = 0
+
+
+while True:
+    if '666' in str(result):
+        count += 1
+
+    if count == N:
+        break
+
+    result += 1
+
+print(result)
