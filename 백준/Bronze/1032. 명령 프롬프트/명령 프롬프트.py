62fedad1cbc3f4aca1fda7f3e2a5da460b810dac
SuyoungPark11 <87575830+SuyoungPark11@users.noreply.github.com>
2025-02-15 12:34:11 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+num = int(input())
+word = list(input())
+
+for _ in range(num-1):
+    word_2 = input()
+    for n in range(len(word)):
+        if word[n] == word_2[n]:
+            continue
+        else:
+            word[n] = "?"
+print(*word, sep = "")
